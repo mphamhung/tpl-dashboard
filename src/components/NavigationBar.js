90c4b1788363c1704a3ff18c182fe8814ee4d76f
Michael Pham-Hung <mphung@Michaels-MacBook-Pro.local>
2023-11-27 23:30:01 -0500

    nav bar

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,7 +3,10 @@ import Link from 'next/link'
 export default function NavigationBar() {
    return (
         <section className="grow justify-between space-y-4">
-            <Link href="/#">Home</Link>
+            <Link href="/#">
+                <h1 className="m-4 align-middle"> 
+                Home
+                </h1></Link>
         </section>
     )
 
