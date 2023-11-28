6b6ca12d012d3f9529bfeb4c604e06fadaf13a7d
Michael Pham-Hung <mphung@Michaels-MBP.phub.net.cable.rogers.com>
2023-11-28 01:01:32 -0500

    player scatter plot

--- src/app/player/[playerId]/page.js
+++ src/app/player/[playerId]/page.js
@@ -1,16 +1,21 @@
 import {getPlayerGameEvents} from '@/lib/api-fetching'
 import gamePlayerEventSequenceToSummaryDict from "@/lib/transforms/GamePlayerEventSequenceToSummaryDict"
 import StatTable from '@/components/PlayerStatTable'
+import { ScatterContributions} from '@/components/Contributions'
 
 export default async function Page({params}) {
     const gameEvents = await getPlayerGameEvents(params.playerId);
     const summaryOfGamesWithPlayer = gamePlayerEventSequenceToSummaryDict(gameEvents)
     const playerStats = Object.keys(summaryOfGamesWithPlayer).map(gameId => {
-      return summaryOfGamesWithPlayer[gameId][params.playerId]
+      var temp_dict= summaryOfGamesWithPlayer[gameId][params.playerId]
+      temp_dict["Name"] = gameId
+      return temp_dict
     })
+    
     return (
       <>
       <StatTable rows={playerStats}/>
+      <ScatterContributions rows={playerStats}/>
       </>
     )
 }
