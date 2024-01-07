da63b389b0e8bda544cf29ce288c120079a1e186
Michael Pham-Hung <mphung@Michaels-MBP.phub.net.cable.rogers.com>
2024-01-06 20:48:50 -0500

    wip

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig =  {
     env: {
       leagueId: '764',
       serverUrl: "https://tplapp.onrender.com/",
+      DATABASE_URL: 'mysql://foszeeyiimlxqi0l5rjt:pscale_pw_Gs6UkFbuZYvE0HabC4SeqvZCZ41RGUpaFCI5TIsFcpW@aws.connect.psdb.cloud/tpl-dashboard-db?ssl={"rejectUnauthorized":true}'
     },
     reactStrictMode: false,
   }
--- src/lib/api-fetching.js
+++ src/lib/api-fetching.js
@@ -1,6 +1,13 @@
 const leagueId = process.env.leagueId
 const serverUrl = process.env.serverUrl
 
+const mysql = require('mysql2')
+
+// Create the connection to the database
+const connection = mysql.createConnection(process.env.DATABASE_URL)
+
+
+
 export async function getGames() {
     const res = await fetch(serverUrl+ 'games/' + leagueId, {cache:'no-store'}) 
     // Revalidate every day
@@ -15,6 +22,18 @@ export async function getGames() {
 
 
 export async function getGameEvents(gameId, teamId) {
+      // simple query
+    connection.query('show tables', function (err, results, fields) {
+      console.log(results) // results contains rows returned by server
+      console.log(fields) // fields contains extra metadata about results, if available
+    })
+
+    // Example with placeholders
+    connection.query('select 1 from dual where ? = ?', [1, 1], function (err, results) {
+      console.log(results)
+    })
+
+
     const res = await fetch(serverUrl+'gameEvents/'+gameId+"/"+teamId, )
     // The return value is *not* serialized
     // You can return Date, Map, Set, etc.
