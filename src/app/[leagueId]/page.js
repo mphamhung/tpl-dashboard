6f0400a18d68e5aefd58339dd4b1ad7a9ba39291
michael <mphamhung@gmail.com>
2024-02-29 18:09:37 -0500

    linting

--- src/app/[leagueId]/page.js
+++ src/app/[leagueId]/page.js
@@ -1,16 +1,18 @@
 import GamesList from "@/components/GamesList";
 import { Suspense } from "react";
-
+/**
+ * Page to display a list of games
+ * @param {Object} params the paramter from routing
+ * @return {*}
+ */
 export default async function Home({ params }) {
   return (
-    <>
-      <div>
-        <main className="flex flex-row flex-grow">
-          <Suspense fallback={<p>Loading...</p>}>
-            <GamesList leagueId={params.leagueId} />
-          </Suspense>
-        </main>
-      </div>
-    </>
+    <div>
+      <main className="flex flex-row flex-grow">
+        <Suspense fallback={<p>Loading...</p>}>
+          <GamesList leagueId={params.leagueId} />
+        </Suspense>
+      </main>
+    </div>
   );
 }
